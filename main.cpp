03b8a200ba8eafadc59e625e0ea41b66d0eb6594
x-syaifullah-x <x.syaifullah.x@gmail.com>
2020-06-27 18:20:01 +0700

    ...

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <filesystem>
 #include <sys/mount.h>
 #include <cstring>
+#include <sys/stat.h>
+#include <unistd.h>
 
 namespace fs = std::filesystem;
 
@@ -9,10 +11,9 @@ namespace fs = std::filesystem;
 void created_directory(const std::string &path) {
     std::string delimiter = "/";
     std::string test;
-    auto start = 0U;
     auto end = path.find(delimiter);
     while (end != std::string::npos) {
-        start = end + delimiter.length();
+        auto start = end + delimiter.length();
         end = path.find(delimiter, start);
         if (start != 0) {
             test += delimiter + path.substr(start, end - start);
@@ -33,25 +34,30 @@ void created_directory(const std::string &path) {
 }
 
 int main(int argc, char *argv[]) {
-    const char *paths[] = {
-            "/tmp/aaaa/sasas",
-    };
+    struct stat st{};
+    std::cout << stat("/home/xxx", &st) << std::endl;
+    std::cout << st.st_uid << std::endl;
 
-    for (auto &path : paths) {
-        created_directory(path);
-        if (argc == 2) {
-            if (strcmp(argv[1], "umount") == 0) {
-                if (umount2(path, 0) != 0) {
-                    std::cout << "umont filed " << path << std::endl;
-                }
-            }
-        } else {
-            if (mount("tmpfs", path, "tmpfs", MS_REMOUNT, "size=5000m") == -1) {
-                if (mount("tmpfs", path, "tmpfs", 0, "size=5000m") != 0) {
-                    std::cout << "mount filed " << path << std::endl;
-                }
-            }
-        }
-    }
+
+//    const char *paths[] = {
+//            "/tmp/aaaa/sasas",
+//    };
+//
+//    for (auto &path : paths) {
+//        created_directory(path);
+//        if (argc == 2) {
+//            if (strcmp(argv[1], "umount") == 0) {
+//                if (umount2(path, 0) != 0) {
+//                    std::cout << "umont filed " << path << std::endl;
+//                }
+//            }
+//        } else {
+//            if (mount("tmpfs", path, "tmpfs", MS_REMOUNT, "size=5000m") == -1) {
+//                if (mount("tmpfs", path, "tmpfs", 0, "size=5000m") != 0) {
+//                    std::cout << "mount filed " << path << std::endl;
+//                }
+//            }
+//        }
+//    }
     return 0;
 }
